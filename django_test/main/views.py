15cb3ef4808f477a2b2a2d411b14b17b0be4708a
Cho Oo Wai <chooowai@icloud.com>
2021-11-16 22:15:55 +0700

    manual filter

--- django_test/main/views.py
+++ django_test/main/views.py
@@ -4,10 +4,19 @@ from rest_framework import status
 
 from .models import School, Student
 from .serializers import SchoolSerializer, StudentSerializer
-
 class SchoolViewSet(ModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
+
+    def get_queryset(self):
+        queryset = School.objects.all()
+        name = self.request.query_params.get('name')
+        max_student = self.request.query_params.get('max_student')
+        if name is not None:
+            queryset = queryset.filter(name=name)
+        if max_student is not None:
+            queryset = queryset.filter(max_student=max_student)
+        return queryset
 
     def update(self, request, pk):
         request.POST._mutable = True
@@ -16,8 +25,24 @@ class SchoolViewSet(ModelViewSet):
         return super().update(request)
 
 class StudentViewSet(ModelViewSet):
-    queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    queryset = Student.objects.all()
+
+    def get_queryset(self):
+        queryset = Student.objects.all()
+        first_name = self.request.query_params.get('first_name')
+        last_name = self.request.query_params.get('last_name')
+        gpa = self.request.query_params.get('gpa')
+        school = self.request.query_params.get('school')
+        if first_name is not None:
+            queryset = queryset.filter(first_name=first_name)
+        if last_name is not None:
+            queryset = queryset.filter(last_name=last_name)
+        if gpa is not None:
+            queryset = queryset.filter(gpa=gpa)
+        if school is not None:
+            queryset = queryset.filter(school=school)
+        return queryset
 
     def update(self, request, pk):
         request.POST._mutable = True
@@ -28,7 +53,17 @@ class StudentViewSet(ModelViewSet):
 class SchoolStudentViewSet(ModelViewSet):
     serializer_class = StudentSerializer
     def get_queryset(self):
-        return Student.objects.filter(school=self.kwargs['school_pk'])
+        queryset = Student.objects.filter(school=self.kwargs['school_pk'])
+        first_name = self.request.query_params.get('first_name')
+        last_name = self.request.query_params.get('last_name')
+        gpa = self.request.query_params.get('gpa')
+        if first_name is not None:
+            queryset = queryset.filter(first_name=first_name)
+        if last_name is not None:
+            queryset = queryset.filter(last_name=last_name)
+        if gpa is not None:
+            queryset = queryset.filter(gpa=gpa)
+        return queryset
 
     def create(self, request, school_pk):
         request.POST._mutable = True
